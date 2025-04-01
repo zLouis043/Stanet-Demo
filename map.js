02a39eb8a81127ba99b337fc8622f6c6ed11276b
zLouis043 <louis.003.lt@gmail.com>
2025-04-01 23:12:33 +0200

    fixed map issue

--- map.js
+++ map.js
@@ -11,14 +11,15 @@ var tile_layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   
 parseCSV(filePath, delimiter)
     .then((data) => {
+        var latlng;
         data.forEach((row) => {
         if(row['Y'] && row['X']){
-            var latlng = new L.latLng(Number(row['Y']), Number(row['X']));
-            map.setView(latlng,9);
-            console.log(latlng);
+            latlng = new L.latLng(Number(row['Y']), Number(row['X']));
             var marker = new L.Marker(latlng).addTo(fg);
         }
         });
+
+        map.setView(latlng,9);
     })
     .catch((error) => {
         console.error("Errore durante il parsing del CSV:", error);
